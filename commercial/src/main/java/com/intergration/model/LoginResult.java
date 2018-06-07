4da7f855fc393ec48f31c4555b22f46f66735c1f
yojay <wuyoujie0208@outlook.com>
2018-06-07 20:05:41 +0800

    delete

--- commercial/src/main/java/com/intergration/model/LoginResult.java
+++
@@ -1,8 +0,0 @@
-package com.intergration.model;
-
-/**
- * Created by 朱应山 on 2018/6/1.
- */
-public enum LoginResult {
-    NAMEERR,NOEXIST,SUCESS
-}
--- software/src/main/java/com/intergration/model/po/CoursePo.java
+++
@@ -1,7 +0,0 @@
-package com.intergration.model.po;
-
-/**
- * Created by 朱应山 on 2018/6/1.
- */
-public class CoursePo {
-}
